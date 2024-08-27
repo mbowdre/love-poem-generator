9c440f380f23d10ca3eb9a295e6d8bdd96168fa5
mbowdre <bowdremaya@gmail.com>
2024-08-27 00:17:34 -0500

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Love is like a flower",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bdtbcb1807e03647e4ofbb3da4003d3b";
+  let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
+  let context =
+    "You are a poet who writes short love poems  in basic HTML. Your goal is to write a poem no longer than 10 lines. Make sure to follow user instructions as the topic of the love poem. Do not include'html'in text.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Contex: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
